d49a268ec738b3fbb0d4d6cda4ffa660978711ee
yhb2010 <youtong82@163.com>
2018-09-13 12:24:08 +0800

    1

--- src/main/java/com/cdel/cassandra/table/CassandraTest.java
+++ src/main/java/com/cdel/cassandra/table/CassandraTest.java
@@ -68,8 +68,23 @@ public class CassandraTest {
 
 	}
 
+	public static void batch(){
+		// 创建客户端
+		CassandraClient cassandraClient = new CassandraClient("test");
+
+		String query =" BEGIN BATCH INSERT INTO emp (emp_id, emp_city, emp_name, emp_phone, emp_sal) values( 4,'Pune','rajeev',9848022331, 30000);"
+	         + "UPDATE emp SET emp_sal = 50000 WHERE emp_id =3;"
+	         + "DELETE emp_city FROM emp WHERE emp_id = 2;"
+	         + "APPLY BATCH;";
+		cassandraClient.executeSQL(query);
+
+		// 关闭
+		cassandraClient.close();
+
+	}
+
 	public static void main(String[] args) {
-		dropIndex();
+		batch();
     }
 
 }
